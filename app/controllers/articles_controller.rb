e3a242378ea2be642c5445d93d54ae4df607bc33
Paddy Urlich <paddyurlich@gmail.com>
2016-03-29 09:19:42 +0000

    add styling to Show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,14 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id]) ==> used private method instead "set_article", in before_action
     end
     
     def update
-        #@article = Article.find(params[:id])
+        #@article = Article.find(params[:id]) ==> used private method instead "set_article", in before_action
         
         if @article.update(article_params)
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -25,23 +25,23 @@ class ArticlesController < ApplicationController
     end
     
     def show
-     #@article = Article.find(params[:id])
+     #@article = Article.find(params[:id]) ==> used private method instead "set_article", in before_action
     end
 
     def destroy
-        #@article = Article.find(params[:id])
+        #@article = Article.find(params[:id]) ==> used private method instead "set_article", in before_action
         
         @article.destroy
-        flash[:notice] = "Article was successfully deleted"
+        flash[:danger] = "Article was successfully deleted"
         redirect_to articles_path
     end
 
     def create
-        #render plain: params[:article].inspect
+        # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
-            #note: flash message code added to application.html.erb 
+            flash[:success] = "Article was successfully created"
+            #note: flash message code added to partial, _messages.html.erb then rendered in application.html.erb 
             redirect_to article_path(@article)
         else
             render'new'
